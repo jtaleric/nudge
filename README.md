f5f2b9d7e3fbc85ce0a3479a8a650ac79d7e7faf
Joe Talerico <jtaleric@redhat.com>
2021-04-27 14:39:19 -0400

    Working bits.

--- README.md
+++ README.md
@@ -1,12 +1,25 @@
-# nudge - JIRA tool to help get nudge people to update their JIRA cards
+# nudge - JIRA tool to help get nudge people
 
-## Setup
-
+## JIRA Setup
 ```
 export JIRA_Server="http://my-JIRA-Server"
 export JIRA_User="MyUserName"
 export JIRA_Pass="MyJIRAPass"
 
-export JIRA_Team_query=""
+export JIRA_Queries=""
 ```
 
+`JIRA_Queries` can be one more many queries to execute. For example:
+
+`project in (PERFSCALE) AND updated <= -3d AND sprint in closedSprints() AND sprint in openSprints() AND status not in (Closed, Done) AND labels in (managed-services, perfscale-managed-services)`
+
+The important part of the query is the `updated <= -3d`, this will return cards that have not been updated the last 3 days.
+
+## Email Reminder setup
+```
+export EMAIL_Server=""
+export EMAIL_From="" # Who the email bot should pretend to be
+export EMAIL_To="" # What list / set of people the bot should send to.
+```
+
+This should be pretty simple to understand.
--- nudge.py
+++ nudge.py
@@ -1,21 +1,45 @@
-import pprint
+"""
+Copyright 2021 Joe Talerico
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import smtplib
 import logger as log
 import jira
 import os
 
+# JIRA Connection Setup
 server=os.environ['JIRA_Server']
 username=os.environ['JIRA_User']
 password=os.environ['JIRA_Pass']
 
+# Queries, Tab delimited
 queries=os.environ['JIRA_Queries'].split('  ')
+
+# EMAIL Nudges
+smtpServer=os.environ['EMAIL_Server']
+smtpFrom=os.environ['EMAIL_From']
+smtpTo=os.environ['EMAIL_To']
 
 options = { 'server': server }
 conn = jira.JIRA(options, basic_auth=(username, password))
 
 issues=[]
 nudges=[]
-
 nudgeMessage=[]
+
+sendEmail=False
 
 if len(queries) < 1:
     log.logger.error("No JIRA queries provided.")
@@ -41,10 +65,17 @@ log.logger.info("Number of nudges: {}".format(len(nudges)))
 
 if len(nudges) > 0 :
     for nudge in nudges:
-        nudgeMessage.append("JIRA ({}) Has not been updated in over 3 days (laste updated){}, please {} provide an update - {}".
-                            format(nudge['JIRA'],
-                                   nudge['UPDATED'],
-                                   nudge['OWNER'],
-                                   nudge['LINK']))
+        nudgeMessage.append("{}\nHas not been updated since {}. Please {} provide an update {}\n".
+                format(nudge['JIRA'],
+                    nudge['UPDATED'].split('T')[0],
+                    nudge['OWNER'],
+                    nudge['LINK']))
 
-print("\n".join(nudgeMessage))
+    if sendEmail :
+    	server = smtplib.SMTP(smtpServer)
+    	msg = "Subject: OCP PerfScale JIRA Nudge\n\n"
+    	msg += "Hello PerfScale Team,\nBelow are the current nudges, please address them today.\n\n"
+    	msg += "{}".format("\n".join(nudgeMessage))
+    	server.sendmail(smtpFrom, smtpTo, msg)
+    	server.quit()
+    	log.logger.info("Email Sent")
